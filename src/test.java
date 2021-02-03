0509a53137033e5b372eeba65aa49fb2bf5e51f9
hutch445 <65507983+hutch445@users.noreply.github.com>
2021-02-02 20:02:44 -0500

    second commit

--- src/test.java
+++ src/test.java
@@ -4,5 +4,9 @@ public class test {
 	{
 		System.out.println("lol");
 		System.out.println("IDK what to put");
+		System.out.println("Please let this work");
+		System.out.println("damn it didnt work");
+		System.out.println("how many times am i gonna have to add something");
+		System.out.println("please");
 	}
 }
