ad3e2813b94b9b1aa7ae60266d87a1e6dcc7dd72
hutch445 <65507983+hutch445@users.noreply.github.com>
2021-04-28 17:09:34 -0400

    Removed all lines and added one back

--- src/test.java
+++ src/test.java
@@ -2,15 +2,6 @@
 public class test {
 	public static void main(String[] args)
 	{
-		System.out.println("lol");
-		System.out.println("IDK what to put");
-		System.out.println("Please let this work");
-		System.out.println("damn it didnt work");
-		System.out.println("how many times am i gonna have to add something");
-		System.out.println("please");
-		System.out.println("I'm just adding stuff to get to the requirements lol");
-		System.out.println("I dont know what to add so im just adding print statements");
-		System.out.println("Hi Professor Thimineur");
-		System.out.println("How much later is this than previous commits?");
+		System.out.println("Switched it up a took away everything lol");
 	}
 }
