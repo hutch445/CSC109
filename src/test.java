edde537cbe1b07a69bb7d163a59ee4851c69f479
hutch445 <65507983+hutch445@users.noreply.github.com>
2021-04-28 17:10:18 -0400

    One last one for good measure

--- src/test.java
+++ src/test.java
@@ -3,5 +3,6 @@ public class test {
 	public static void main(String[] args)
 	{
 		System.out.println("Switched it up a took away everything lol");
+		System.out.println("How many commits did I need again? This should be enough");
 	}
 }
